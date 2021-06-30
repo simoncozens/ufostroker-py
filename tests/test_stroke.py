6a8828022dc2561c9370560722c5cabe8034af58
Simon Cozens <simon@simon-cozens.org>
2021-06-30 09:23:09 +0100

    Testspectations

--- tests/test_stroke.py
+++ tests/test_stroke.py
@@ -1,10 +1,14 @@
 import ufoLib2
 f = ufoLib2.Font("tests/data/Scurve.ufo")
-a = f["C"]
 
 def test_cws():
 	from ufostroker import constant_width_stroke
+	a = f["A"]
 	constant_width_stroke(a, 100)
-	# assert len(a) == 1
-	# assert len(a[0]) == 44
-	f.save("CWS.ufo")
+	assert len(a) == 1
+	assert len(a[0]) == 36
+
+	b = f["B"]
+	constant_width_stroke(b, 100)
+	assert len(b) == 4
+	assert len(b[0]) == 12
