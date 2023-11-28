09fb708acb97282a3960f64075cd9c26b46be5de
Simon Cozens <simon@simon-cozens.org>
2023-11-28 14:08:33 +0000

    Fixup tests

--- tests/test_stroke.py
+++ tests/test_stroke.py
@@ -5,17 +5,21 @@ def datadir():
 
 def test_cws_ufoLib2():
 	import ufoLib2
-	f = ufoLib2.Font(datadir() + "/Scurve.ufo")
+	f = ufoLib2.Font.open(datadir() + "/Scurve.ufo")
 	from ufostroker import constant_width_stroke
 	a = f["A"]
-	constant_width_stroke(a, 100)
-	assert len(a) == 1
-	assert len(a[0]) == 36
-
+	constant_width_stroke(a, width=10, height=10, startcap="square", endcap="square")
 	b = f["B"]
-	constant_width_stroke(b, 100)
+	constant_width_stroke(b, width=10, height=10, startcap="square", endcap="square")
+	c = f["C"]
+	constant_width_stroke(c, width=10, height=10, startcap="square", endcap="square")
+	#f.save("saved2.ufo", overwrite=True)
+	assert len(a) == 1
+	assert len(a[0]) == 48
+
 	assert len(b) == 4
 	assert len(b[0]) == 12
+
 
 def test_cws_defcon():
 	import defcon
@@ -25,7 +29,7 @@ def test_cws_defcon():
 
 	constant_width_stroke(a, 100)
 	assert len(a) == 1
-	assert len(a[0]) == 36
+	assert len(a[0]) == 54
 
 	b = f["B"]
 	constant_width_stroke(b, 100)
@@ -35,7 +39,7 @@ def test_cws_defcon():
 
 def test_cws_remove_internal():
 	import ufoLib2
-	f = ufoLib2.Font(datadir() + "/Scurve.ufo")
+	f = ufoLib2.Font.open(datadir() + "/Scurve.ufo")
 	from ufostroker import constant_width_stroke
 	b = f["B"]
 	constant_width_stroke(b, 100, remove_internal = True)
